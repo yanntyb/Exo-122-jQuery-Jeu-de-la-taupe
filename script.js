a7f3bb6267e8e5b52c6a996b545cd28f03491712
yanntyb <69966391+yanntyb@users.noreply.github.com>
2021-01-12 12:48:22 +0100

    a

--- script.js
+++ script.js
@@ -1,3 +1,50 @@
-const holes = // utilier la méthode jquery appropriée pour selectionner les éléments ayant pour classe "hole";
-const scoreBoard = //utiliser la méthode jquery appropriée pour selectionner l'élément ayant pour classe "score";
-const moles = // utilier la méthode jquery appropriée pour selectionner les éléments ayant pour classe "mole";
+const holes = $(".hole");// utilier la méthode jquery appropriée pour selectionner les éléments ayant pour classe "hole";
+const scoreBoard = $(".score");//utiliser la méthode jquery appropriée pour selectionner l'élément ayant pour classe "score";
+const moles = $(".mole");// utilier la méthode jquery appropriée pour selectionner les éléments ayant pour classe "mole";
+
+let score = 0;
+
+function startGame() {
+    let timer = 10;
+    function timeFunc() {
+        setTimeout(function () {
+            let time = Math.floor((Math.random() * (600))) + 200;
+            let elem = $(".mole").get()[Math.trunc(Math.random() * $(".mole").get().length)];
+            if(!$(elem).attr("class").includes("active")){
+                $(elem).addClass("active");
+                $(elem).animate({
+                    top: "20%",
+                }, time)
+                    .animate({
+                        top: "100%",
+                    }, time);
+                setTimeout(function(){$(elem).removeClass("active")},time*2);
+
+            };
+            if(timer >0){
+                timeFunc();
+            }
+        },Math.floor(Math.random() * 2000) + 500);
+    }
+
+    function timerChange(){
+        setTimeout(function (){
+            timer--;
+            if(timer > 0){
+                timerChange();
+            }
+            console.log(timer);
+        },1000)
+    }
+    timeFunc();
+    timerChange();
+}
+
+
+document.getElementById("startGame").addEventListener("click",startGame);
+
+moles.click(() => {
+    score++;
+    $(this).removeClass("up");
+    document.getElementsByClassName("score")[0].innerHTML = score;
+});
