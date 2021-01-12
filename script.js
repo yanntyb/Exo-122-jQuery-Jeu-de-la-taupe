1749dd0f65f4a473b92dfab73edd1d75e4fcb266
yanntyb <69966391+yanntyb@users.noreply.github.com>
2021-01-12 13:10:33 +0100

    a

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-const holes = $(".hole");// utilier la méthode jquery appropriée pour selectionner les éléments ayant pour classe "hole";
-const scoreBoard = $(".score");//utiliser la méthode jquery appropriée pour selectionner l'élément ayant pour classe "score";
 const moles = $(".mole");// utilier la méthode jquery appropriée pour selectionner les éléments ayant pour classe "mole";
 
 let score = 0;
